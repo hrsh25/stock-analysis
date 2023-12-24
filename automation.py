2375437355f47791c5bb0644395f007e71b59bef
hrsh25 <harshvr98@gmail.com>
2023-12-24 15:01:14 -0500

    Initial commit

--- automation.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.google.com")
-    browser.close()
-
